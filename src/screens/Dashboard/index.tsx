1851ac904a71ffe83c33102287bf7f07eace54c5
Paulo Guilherme <paulopgss@gmail.com>
2021-06-09 01:46:57 -0300

    Tema global e dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, Title } from './styles';
 
 export function Dashboard() {
-  return <View />;
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
 }
